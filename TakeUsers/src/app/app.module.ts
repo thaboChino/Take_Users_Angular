32b8d70a3a63b79179b089ea3ceee5b1c3df1949
Ntalapro <ncosanetshepiso@gmail.com>
2023-11-22 12:02:07 +0200

    first commit

---
+++ TakeUsers/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateuserComponent } from './createuser/createuser.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateuserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
